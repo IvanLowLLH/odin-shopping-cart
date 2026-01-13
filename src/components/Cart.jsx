ee518004948fff6f07ce6ccf1c9440cd538738e6
IvanLowLLH <ivanlowlh@gmail.com>
2026-01-13 15:12:31 +0800

    Add basic display of cart items in Cart page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,22 @@
+import { useOutletContext } from "react-router";
+
 export default function Cart() {
+  const { cart } = useOutletContext();
   return (
     <div className="cart-page">
-      <h1>Cart Placeholder</h1>
+      <h1>Your Cart</h1>
+      {cart.length == 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <div className="cart-list">
+          {cart.map((item) => (
+            <div className="cart-item">
+              <p>{item.title}</p>
+              <p>{item.qty}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
