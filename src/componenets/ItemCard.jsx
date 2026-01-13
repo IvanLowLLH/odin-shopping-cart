bd5ac4ecb85567757927b34af7b83c2382b0efd6
IvanLowLLH <ivanlowlh@gmail.com>
2026-01-13 11:43:32 +0800

    Add qty input and buttons

--- src/componenets/ItemCard.jsx
+++ src/componenets/ItemCard.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import styles from "./ItemCard.module.css";
 
 function ItemCard({ itemInfo }) {
+  const [itemCount, setItemCount] = useState(0);
+
+  const incrementCount = () => {
+    setItemCount(itemCount + 1);
+  };
+  const decrementCount = () => {
+    if (itemCount === 0) {
+      return;
+    }
+    setItemCount(itemCount - 1);
+  };
   return (
     <div className={styles["item-card"]}>
       <div className={styles["img-card-container"]}>
@@ -9,6 +21,17 @@ function ItemCard({ itemInfo }) {
       <div className={styles["item-info"]}>
         <h1 className={styles["item-title"]}>{itemInfo.title}</h1>
         <p className={styles["item-price"]}>${itemInfo.price}</p>
+      </div>
+      <div className={styles["item-qty-interface"]}>
+        <button onClick={incrementCount}>+</button>
+        <input
+          type="text"
+          inputmode="numeric"
+          pattern="[0-9]*"
+          value={itemCount}
+          onChange={(e) => setItemCount(e.target.value)}
+        ></input>
+        <button onClick={decrementCount}>-</button>
       </div>
     </div>
   );
