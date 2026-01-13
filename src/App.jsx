6dbaf5ef2d438e46cc97781aa6c2a923e16f6066
IvanLowLLH <ivanlowlh@gmail.com>
2026-01-13 15:11:55 +0800

    Add state and context for cart in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import { Outlet } from "react-router";
+import { useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((item) => item.id === product.id);
+      if (existingItem) {
+        return prevCart.map((item) =>
+          item.id === product.id ? { ...item, qty: item.qty + 1 } : item,
+        );
+      }
+      return [...prevCart, { ...product, qty: 1 }];
+    });
+  };
   return (
     <>
       <NavBar />
       <main>
-        <Outlet />
+        <Outlet context={{ cart, addToCart }} />
       </main>
     </>
   );
