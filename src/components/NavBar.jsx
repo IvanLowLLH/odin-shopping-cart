0e81d904d7051769ab89ec8a199e7d8195706f13
IvanLowLLH <ivanlowlh@gmail.com>
2026-01-13 13:44:14 +0800

    Add basic nav bar and styles

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router";
+import styles from "./NavBar.module.css";
+
+export default function NavBar() {
+  return (
+    <div className={styles["nav-bar"]}>
+      <div className={styles["nav-logo"]}>
+        <h1>Placeholder</h1>
+      </div>
+      <div className={styles["nav-links"]}>
+        <Link to="/">Home</Link>
+        <Link to="/shop">Shop</Link>
+        <Link to="/cart">Cart</Link>
+      </div>
+    </div>
+  );
+}
